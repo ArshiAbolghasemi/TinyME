9c0fda9af8a39008383e3f584a59c79fb1c26478
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2024-04-10 16:05:17 +0330

    style: renaming msg constant and move logic range meq to method for more clarity

--- src/main/java/ir/ut/se/tinyme/messaging/Message.java
+++ src/main/java/ir/ut/se/tinyme/messaging/Message.java
@@ -14,5 +14,5 @@ public class Message {
     public static final String QUANTITY_NOT_MULTIPLE_OF_LOT_SIZE = "Quantity is not a multiple of security lot size";
     public static final String PRICE_NOT_MULTIPLE_OF_TICK_SIZE = "Price is not a multiple of security tick size";
     public static final String SELLER_HAS_NOT_ENOUGH_POSITIONS = "Seller has not enough positions";
-    public static final String INVALID_MINIMUM_EXECUTION_QUANTITY = "Minimum Execution Quantity is out of range";
+    public static final String INVALID_MINIMUM_EXECUTION_QUANTITY_RANGE = "Minimum Execution Quantity is out of range";
 }
