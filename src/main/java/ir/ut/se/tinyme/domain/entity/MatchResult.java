0ab3fbb07d4937c614f75e41c9b26e929e1e3731
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2024-04-10 20:52:21 +0330

    feat: adding match result not met meq

--- src/main/java/ir/ut/se/tinyme/domain/entity/MatchResult.java
+++ src/main/java/ir/ut/se/tinyme/domain/entity/MatchResult.java
@@ -19,6 +19,11 @@ public final class MatchResult {
     public static MatchResult notEnoughPositions() {
         return new MatchResult(MatchingOutcome.NOT_ENOUGH_POSITIONS, null, new LinkedList<>());
     }
+
+    public static MatchResult minimumExecutionQuantityNotMet() {
+        return new MatchResult(MatchingOutcome.MINIMUM_EXECUTION_QUANTITY_NOT_MET, null, new LinkedList<>());
+    }
+
     private MatchResult(MatchingOutcome outcome, Order remainder, LinkedList<Trade> trades) {
         this.outcome = outcome;
         this.remainder = remainder;
