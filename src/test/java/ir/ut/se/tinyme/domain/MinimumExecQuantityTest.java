e4231706e67f6a85ed32a3c4df50a918c23f80ff
shahab <shahabrakhsha@gmail.com>
2024-04-10 21:05:32 +0330

    added the base test file

---
+++ src/test/java/ir/ut/se/tinyme/domain/MinimumExecQuantityTest.java
@@ -0,0 +1,43 @@
+package ir.ut.se.tinyme.domain;
+
+import ir.ut.se.tinyme.domain.entity.*;
+import ir.ut.se.tinyme.domain.service.Matcher;
+import ir.ut.se.tinyme.domain.service.OrderHandler;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MinimumExecQuantityTest {
+    private Security security;
+    private Broker broker;
+    private Shareholder shareholder;
+    private OrderBook orderBook;
+    private List<Order> orders;
+    private Matcher matcher;
+    private OrderHandler orderHandler;
+
+
+    @BeforeEach
+    void setupOrderBook() {
+        security = Security.builder().build();
+        broker = Broker.builder().credit(100_000_000L).build();
+        shareholder = Shareholder.builder().build();
+        shareholder.incPosition(security, 100_000);
+        orderBook = security.getOrderBook();
+        orders = Arrays.asList(
+                new Order(1, security, Side.BUY, 304, 15700, broker, shareholder),
+                new Order(2, security, Side.BUY, 43, 15500, broker, shareholder),
+                new Order(3, security, Side.BUY, 445, 15450, broker, shareholder),
+                new Order(4, security, Side.BUY, 526, 15450, broker, shareholder),
+                new Order(5, security, Side.BUY, 1000, 15400, broker, shareholder),
+                new Order(6, security, Side.SELL, 350, 15800, broker, shareholder),
+                new Order(7, security, Side.SELL, 285, 15810, broker, shareholder),
+                new Order(8, security, Side.SELL, 800, 15810, broker, shareholder),
+                new Order(9, security, Side.SELL, 340, 15820, broker, shareholder),
+                new Order(10, security, Side.SELL, 65, 15820, broker, shareholder)
+        );
+        orders.forEach(order -> orderBook.enqueue(order));
+    }
+}
