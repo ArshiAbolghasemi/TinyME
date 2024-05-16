8b7b2db300c813a78fa3668e679f6bae4b7f0a9a
shahabrakhsha <shahabrakhsha@gmail.com>
2024-05-16 12:21:29 +0330

    feat: added MatchRqHandler and updated the dispatcher

---
+++ src/main/java/ir/ut/se/tinyme/domain/service/MatcherHandler.java
@@ -0,0 +1,28 @@
+package ir.ut.se.tinyme.domain.service;
+
+import ir.ut.se.tinyme.messaging.EventPublisher;
+import ir.ut.se.tinyme.messaging.request.MatchingStateRq;
+import ir.ut.se.tinyme.repository.BrokerRepository;
+import ir.ut.se.tinyme.repository.SecurityRepository;
+import ir.ut.se.tinyme.repository.ShareholderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MatcherHandler {
+    SecurityRepository securityRepository;
+    BrokerRepository brokerRepository;
+    ShareholderRepository shareholderRepository;
+    EventPublisher eventPublisher;
+
+   public MatcherHandler(SecurityRepository securityRepository, BrokerRepository brokerRepository,
+                         ShareholderRepository shareholderRepository, EventPublisher eventPublisher) {
+       this.securityRepository = securityRepository;
+       this.brokerRepository = brokerRepository;
+       this.shareholderRepository = shareholderRepository;
+       this.eventPublisher = eventPublisher;
+   }
+
+    public void handleMatchStateRq(MatchingStateRq matchingStateRq){
+
+    }
+}
--- src/main/java/ir/ut/se/tinyme/messaging/RequestDispatcher.java
+++ src/main/java/ir/ut/se/tinyme/messaging/RequestDispatcher.java
@@ -1,8 +1,10 @@
 package ir.ut.se.tinyme.messaging;
 
+import ir.ut.se.tinyme.domain.service.MatcherHandler;
 import ir.ut.se.tinyme.messaging.request.DeleteOrderRq;
 import ir.ut.se.tinyme.messaging.request.EnterOrderRq;
 import ir.ut.se.tinyme.domain.service.OrderHandler;
+import ir.ut.se.tinyme.messaging.request.MatchingStateRq;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
@@ -12,15 +14,23 @@ import java.util.logging.Logger;
 public class RequestDispatcher {
     private final Logger log = Logger.getLogger(this.getClass().getName());
     private final OrderHandler orderHandler;
+    private final MatcherHandler matcherHandler;
 
-    public RequestDispatcher(OrderHandler orderHandler) {
+    public RequestDispatcher(OrderHandler orderHandler, MatcherHandler matcherHandler) {
         this.orderHandler = orderHandler;
+        this.matcherHandler = matcherHandler;
     }
 
     @JmsListener(destination = "${requestQueue}", selector = "_type='ir.ramtung.tinyme.messaging.request.EnterOrderRq'")
     public void receiveEnterOrderRq(EnterOrderRq enterOrderRq) {
         log.info("Received message: " + enterOrderRq);
         orderHandler.handleEnterOrder(enterOrderRq);
+    }
+
+    @JmsListener(destination = "${requestQueue}", selector = "_type='ir.ramtung.tinyme.messaging.request.MatchingStateRq'")
+    public void receiveChangeMatchingStateRq(MatchingStateRq matchingStateRq) {
+        log.info("Received message: " + matchingStateRq);
+        matcherHandler.handleMatchStateRq(matchingStateRq);
     }
 
     @JmsListener(destination = "${requestQueue}", selector = "_type='ir.ramtung.tinyme.messaging.request.DeleteOrderRq'")
